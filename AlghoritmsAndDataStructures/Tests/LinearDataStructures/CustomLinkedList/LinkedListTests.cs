7e3f34a5062950dcf041595bb19283676236ea30
Ruslan Rasengan <ruslanrasengan@gmail.com>
2024-02-02 16:33:33 +0200

    added xUnit Tests
    added new project with unit tests

---
+++ AlghoritmsAndDataStructures/Tests/LinearDataStructures/CustomLinkedList/LinkedListTests.cs
@@ -0,0 +1,47 @@
+﻿using LinearDataStructures.CustomLinkedList;
+
+namespace Tests.LinearDataStructures.CustomLinkedList
+{
+    public class LinkedListTests
+    {
+        [Fact]
+        public void AddFirst_AddsElementToEmptyList_SetsHeadToElement()
+        {
+            //Arrange
+            var list = new LinkedList();
+            int elementToAdd = 1;
+
+            //Act
+            list.AddFirst(elementToAdd);
+
+            //Assert
+            Assert.NotNull(list.Head);
+            Assert.Equal(elementToAdd, list.Head.Data);
+        }
+        [Fact]
+        public void PrintAllNodes_AddElementsToEmptyList_PrintAllElementsFromList()
+        {
+            var list = new LinkedList();
+
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddFirst(3);
+
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            //Act
+
+            list.PrintAllNodes();
+
+            //Assert
+            var exceptedOutput = 
+                "3" + Environment.NewLine + 
+                "2" + Environment.NewLine + 
+                "1" + Environment.NewLine;
+
+            Assert.NotNull (list.Head);
+            Assert.Equal (exceptedOutput, output.ToString());
+        }
+    }
+}
