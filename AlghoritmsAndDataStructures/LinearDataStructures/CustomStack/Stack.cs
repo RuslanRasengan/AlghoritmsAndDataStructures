a010a4ffe207cad4b7f35971271326af205ac6a0
Ruslan Rasengan <ruslanrasengan@gmail.com>
2024-02-02 17:26:22 +0200

    added custom stack
    - added class stack with methods(push,pop,isEmpty,Count,Peek)

---
+++ AlghoritmsAndDataStructures/LinearDataStructures/CustomStack/Stack.cs
@@ -0,0 +1,48 @@
+﻿namespace LinearDataStructures.CustomStack
+{
+    public class Stack<T>
+    {
+        private T[] stackArray;
+        private int maxSize;
+        private int top;
+
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new T(maxSize);
+            top = -1;
+        }
+
+        public void Push(T item)
+        {
+            if (top < maxSize - 1 )
+            {
+                stackArray[++top] = item;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack overflow");
+            }
+        }
+
+        public T Pop()
+        {
+
+        }
+
+        public T Peek()
+        {
+
+        }
+
+        public bool isEmpty()
+        {
+            return top == -1;
+        }
+
+        public int Count()
+        {
+            return top + 1;
+        }
+    }
+}
